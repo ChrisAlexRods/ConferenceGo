6eb14fe560510e4c3c9ce7acf76083b57d4029cd
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-01-17 20:44:16 -0500

    Conference Form in React

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,14 +2,15 @@ import Nav from './Nav';
 import React from 'react';
 import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
-
+import ConferenceForm from './ConferenceForm.js';
 
 function App(props) {
   return (
     <React.Fragment>
       <Nav />
       <div className="container">
-      <LocationForm />
+        <ConferenceForm/>
+      {/* <LocationForm /> */}
         {/* <AttendeesList attendees ={props.attendees} /> */}
       </div>
     </React.Fragment>
