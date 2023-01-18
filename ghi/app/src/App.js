8afa5dd70eaaea48efa8422670c3fd35054c1728
Christian Rodriguez <105233007+ChrisAlexRods@users.noreply.github.com>
2023-01-17 19:38:13 -0500

    Location and Attendee React Forms

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,29 +1,16 @@
 import Nav from './Nav';
 import React from 'react';
+import AttendeesList from './AttendeesList';
+import LocationForm from './LocationForm';
+
 
 function App(props) {
   return (
     <React.Fragment>
       <Nav />
       <div className="container">
-        <table className="table table-dark table-striped">
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Conference</th>
-            </tr>
-          </thead>
-          <tbody>
-          {props.attendees && props.attendees.map(attendee => {
-            return (
-              <tr key={attendee.href}>
-                <td>{ attendee.name }</td>
-                <td>{ attendee.conference }</td>
-              </tr>
-            );
-          })}
-          </tbody>
-        </table>
+      <LocationForm />
+        {/* <AttendeesList attendees ={props.attendees} /> */}
       </div>
     </React.Fragment>
   );
